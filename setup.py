36f568d3a4125b726913ab6c2245555f754f2a0f
KCLucas <108404460+KCLucas@users.noreply.github.com>
2023-09-26 17:29:33 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='OctoPrint-LEDControl',
+    version='1.0.0',
+    packages=['octoprint_LEDControl'],
+    url='https://github.com/KCLucas/OctoPrint-LEDControl',
+    license='MIT',
+    author='KCLucas',
+    author_email='lucasmergner@web.de',
+    description='OctoPrint plugin to control LED strip',
+    include_package_data=True,
+)
